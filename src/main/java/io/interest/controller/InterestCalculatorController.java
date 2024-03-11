03c4db342c7e96c3fe296968632b78c65506da57
PryceTag <dpryce7@gmail.com>
2024-03-11 12:10:11 -0400

    Second commit.

--- src/main/java/io/interest/controller/InterestCalculatorController.java
+++ src/main/java/io/interest/controller/InterestCalculatorController.java
@@ -5,6 +5,7 @@ import io.interest.service.InterestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class InterestCalculatorController {
     @GetMapping("/interest")
     public ResponseEntity<String> getInterest(@RequestParam AccountTypes type, @RequestParam double balance) {
         String response = interestService.displayService(type, balance);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/interest")
+    public ResponseEntity<String> postInterestRate(@RequestParam AccountTypes type, @RequestParam double rate) {
+        String response = interestService.postRate(type, rate);
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/io/interest/service/InterestService.java
+++ src/main/java/io/interest/service/InterestService.java
@@ -1,6 +1,8 @@
 package io.interest.service;
 
 import io.interest.AccountTypes;
+import io.interest.model.Interest;
+import io.interest.repository.InterestRepository;
 import io.interest.strategy.InterestCalculationStrategy;
 import io.interest.strategy.InterestCalculationStrategyFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ public class InterestService {
     @Autowired
     InterestCalculationStrategyFactory interestCalculationStrategyFactory;
 
+    @Autowired
+    private InterestRepository interestRepository;
+
     public String displayService(AccountTypes type, double balance) {
         InterestCalculationStrategy strategy = interestCalculationStrategyFactory.getInterestCalculationStrategy(type);
         double interest = strategy.calculateInterest(balance);
@@ -21,5 +26,17 @@ public class InterestService {
                 "Account Type: " + type + "\n" +
                 "Balance: $" + balance + "\n" +
                 "Interest: $" + interest;
+    }
+
+    public String postRate(AccountTypes type, double rate) {
+        Interest interest = interestRepository.findByType(type.name());
+        interest.setRate(rate);
+        interestRepository.save(interest);
+
+        return  "RATE UPDATE:" + "\n" +
+                "================" + "\n" +
+                "Account Type: " + type + "\n" +
+                "Rate: " + interest.getRate() + "\n";
+
     }
 }
